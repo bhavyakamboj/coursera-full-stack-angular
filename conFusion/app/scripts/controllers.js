a45d38b25b5413bbef8c841fdf165988d1e61ac6
Bhavya Kamboj <bhavyakamboj@gmail.com>
2016-05-21 14:23:05 +0530

    week 4 exercise 2 Handling errors in client-server communication using $http

--- conFusion/app/scripts/controllers.js
+++ conFusion/app/scripts/controllers.js
@@ -7,12 +7,17 @@ angular.module('confusionApp')
         $scope.tab = 1;
         $scope.filtText = '';
         $scope.showDetails = false;
-
-        $scope.dishes= [];
-        menuFactory.getDishes()
+        $scope.showMenu = false;
+        $scope.message = "Loading ...";
+        $scope.dishes= {};
+            menuFactory.getDishes()
             .then(
                 function(response) {
                     $scope.dishes = response.data;
+                    $scope.showMenu = true;
+                },
+                function(response) {
+                    $scope.message = "Error: "+response.status + " " + response.statusText;
                 }
             );
 
@@ -76,11 +81,17 @@ angular.module('confusionApp')
     .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
 
         $scope.dish = {};
+        $scope.showDish = false;
+        $scope.message = "Loading ...";
+
         menuFactory.getDish(parseInt($stateParams.id,10))
             .then(
                 function(response) {
                     $scope.dish = response.data;
                     $scope.showDish = true;
+                },
+                function(response) {
+                    $scope.message = "Error: "+response.status + " " + response.statusText;
                 }
             );
 
@@ -114,6 +125,9 @@ angular.module('confusionApp')
                 function(response){
                     $scope.dish = response.data;
                     $scope.showDish = true;
+                },
+                function(response) {
+                    $scope.message = "Error: "+response.status + " " + response.statusText;
                 }
             );
     }])
